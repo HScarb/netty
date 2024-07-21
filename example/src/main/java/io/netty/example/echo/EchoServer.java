3f65c92139198b2f004fc44f998b26a89eee693b
ScarbWin <jjhfen00@163.com>
2024-07-22 02:24:54 +0800

    update

--- example/src/main/java/io/netty/example/echo/EchoServer.java
+++ example/src/main/java/io/netty/example/echo/EchoServer.java
@@ -47,14 +47,27 @@ public final class EchoServer {
         final EchoServerHandler serverHandler = new EchoServerHandler();
         try {
             ServerBootstrap b = new ServerBootstrap();
+            /*
+            netty有两种Channel类型：
+            一种是服务端用于监听绑定端口地址的NioServerSocketChannel
+            一种是用于客户端通信的NioSocketChannel。
+            每种Channel类型实例都会对应一个PipeLine用于编排对应channel实例上的IO事件处理逻辑。
+            PipeLine中组织的就是ChannelHandler用于编写特定的IO处理逻辑。
+
+            ServerBootstrap启动类方法带有child前缀的均是设置客户端NioSocketChannel属性的。
+             */
             // 配置主从 Reactor
             b.group(bossGroup, workerGroup)
                 // 配置主 Reactor 中的服务端 channel 类型，NioServerSocketChannel 是 JDK NIO 中 ServerSocketChannel 的封装
              .channel(NioServerSocketChannel.class)
                 // 设置服务端 ServerSocketChannel 的 SocketOption，SO_BACKLOG 表示服务端接受客户端连接的队列长度
              .option(ChannelOption.SO_BACKLOG, 100)
+                // 设置服务端 ServerSocketChannel 中对应 Pipeline 中的 ChannelHandler
              .handler(new LoggingHandler(LogLevel.INFO))
+                // 设置客户端 NioSocketChannel 中对应 Pipeline 中的 ChannelHandler
              .childHandler(new ChannelInitializer<SocketChannel>() {
+                 // ChannelInitializer 是当 SocketChannel 成功注册到绑定的 Reactor 上后，用于初始化该 SocketChannel 的 Pipeline
+                 // initChannel 方法在注册成功后执行
                  @Override
                  public void initChannel(SocketChannel ch) throws Exception {
                      ChannelPipeline p = ch.pipeline();
@@ -66,12 +79,15 @@ public final class EchoServer {
                  }
              });
 
+            // 绑定端口地址，开始监听客户端连接事件 OP_ACCEPT
             // Start the server.
             ChannelFuture f = b.bind(PORT).sync();
 
+            // 等待服务端 NioServerSocketChannel 关闭
             // Wait until the server socket is closed.
             f.channel().closeFuture().sync();
         } finally {
+            // 优雅关闭主从 Reactor 线程组
             // Shut down all event loops to terminate all threads.
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
